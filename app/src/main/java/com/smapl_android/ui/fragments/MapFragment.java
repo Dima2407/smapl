2fc31fc2da68152b624f1ff35afe8b18b5b95eaa
Nikolay Dymura <nikolay.dymura@gmail.com>
2017-06-11 10:16:57 +0300

    Вид LoginFragment сделан в соответствии с дизайном. Сделан макет MapFragment

--- app/src/main/java/com/smapl_android/ui/fragments/MapFragment.java
+++ app/src/main/java/com/smapl_android/ui/fragments/MapFragment.java
@@ -2,17 +2,44 @@ package com.smapl_android.ui.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.smapl_android.R;
+import com.smapl_android.model.User;
 
 public class MapFragment extends BaseFragment {
+
+    private static final String TAG = MapFragment.class.getSimpleName();
+    private TextView textUserName;
+    private TextView textCar;
+    private ImageView imageCarPhoto;
+    private User user;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        user = getArguments().getParcelable("user");
+
+        textUserName = (TextView) view.findViewById(R.id.text_map_user_name);
+        textCar = (TextView) view.findViewById(R.id.text_map_car_brand_model);
+        imageCarPhoto = (ImageView) view.findViewById(R.id.img_map_car_photo);
+
+        textUserName.setText(user.getName());
+        Log.i("model", " name = " + textUserName.getText().toString());
+        textCar.setText(user.getCarBrand() + " " + user.getCarModel());
+        Log.i("model", " car model = " + textCar.getText().toString());
+        imageCarPhoto.setImageBitmap(user.getCarPhoto());
     }
 }
