29a931d9a6dc215a7ab9b63cffe64fed351b1599
Nikolay Dymura <nikolay.dymura@gmail.com>
2017-06-14 14:27:11 +0300

    The responses of the server for Login and Registration in accordance with the current API was rewritten. DummyNetworkService in accordance with new Login and Reristration responses was rewritten.

--- app/src/main/java/com/smapl_android/net/responses/RegistrationResponse.java
+++ app/src/main/java/com/smapl_android/net/responses/RegistrationResponse.java
@@ -4,25 +4,102 @@ import com.google.gson.annotations.SerializedName;
 
 public class RegistrationResponse {
 
-    @SerializedName("token")
-    private String token;
+    @SerializedName("first_name")
+    private String firstName;
 
-    @SerializedName("userId")
-    private String userId;
+    @SerializedName("last_name")
+    private String lastName;
 
-    public String getToken() {
-        return token;
+    @SerializedName("mobile_number")
+    private String mobileNumber;
+
+    @SerializedName("car_year")
+    private Integer carYear;
+
+    @SerializedName("car_mark")
+    private String carMark;
+
+    @SerializedName("car_model")
+    private String carModel;
+
+    @SerializedName("car_color")
+    private String carColor;
+
+    @SerializedName("created_at")
+    private String createdAt;
+
+    @SerializedName("updated_at")
+    private String updatedAt;
+
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setToken(String token) {
-        this.token = token;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    public String getUserId() {
-        return userId;
+    public String getLastName() {
+        return lastName;
     }
 
-    public void setUserId(String userId) {
-        this.userId = userId;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public Integer getCarYear() {
+        return carYear;
+    }
+
+    public void setCarYear(Integer carYear) {
+        this.carYear = carYear;
+    }
+
+    public String getCarMark() {
+        return carMark;
+    }
+
+    public void setCarMark(String carMark) {
+        this.carMark = carMark;
+    }
+
+    public String getCarModel() {
+        return carModel;
+    }
+
+    public void setCarModel(String carModel) {
+        this.carModel = carModel;
+    }
+
+    public String getCarColor() {
+        return carColor;
+    }
+
+    public void setCarColor(String carColor) {
+        this.carColor = carColor;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
     }
 }
---
+++ app/src/test/java/com/smapl_android/ApiUnitTest.java
@@ -0,0 +1,46 @@
+package com.smapl_android;
+
+import com.smapl_android.net.NetworkService;
+import com.smapl_android.net.NetworkServiceFactory;
+import com.smapl_android.net.responses.LoginResponse;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ApiUnitTest {
+
+    private static final int WAIT_TIMEOUT = 2;
+
+    private NetworkService networkService;
+
+    @Before
+    public void initNetwork() {
+        networkService = NetworkServiceFactory.create(true);
+    }
+
+    @Test
+    public void checkLoginSuccess() throws Exception {
+        CountDownLatch latch = new CountDownLatch(1);
+        networkService.login("1111111111", "qwerty", new NetworkService.OnResultCallback<LoginResponse, Throwable>() {
+            @Override
+            public void onResult(LoginResponse result, Throwable error) {
+                assertThat(error, CoreMatchers.nullValue());
+                assertThat(result, CoreMatchers.notNullValue());
+                assertThat(result.getResult(), CoreMatchers.allOf(CoreMatchers.notNullValue()));
+            }
+        });
+        latch.await(WAIT_TIMEOUT, TimeUnit.SECONDS);
+    }
+}
