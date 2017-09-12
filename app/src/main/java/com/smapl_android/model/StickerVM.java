1a5b51fafdae297ecb10805028dc5840e683bd5e
Nikolay Dymura <nikolay.dymura@gmail.com>
2017-09-12 17:33:40 +0300

    Fixed campaign view

---
+++ app/src/main/java/com/smapl_android/model/StickerVM.java
@@ -0,0 +1,10 @@
+package com.smapl_android.model;
+
+import android.databinding.ObservableField;
+
+public class StickerVM {
+
+    public final ObservableField<String> photoLeft = new ObservableField<>();
+
+    public final ObservableField<String> photoRight = new ObservableField<>();
+}
